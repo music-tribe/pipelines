9228d1e9c937396e48db01ca9641db6f631e2890
John Hughes <john.hughes@musictribe.com>
2023-04-06 11:34:52 +0100

    refactor: :recycle: refactor the generate functions to include the ability to generate to a buffered channel

--- generator.go
+++ generator.go
@@ -4,15 +4,23 @@ import "context"
 
 func GenerateFromSlice[T any](ctx context.Context, list []T) <-chan T {
 	outStream := make(chan T)
-	go func() {
-		defer close(outStream)
-		for _, item := range list {
-			select {
-			case <-ctx.Done():
-				return
-			case outStream <- item:
-			}
-		}
-	}()
+	go generate(ctx, list, outStream)
 	return outStream
+}
+
+func GenerateFromSliceBuffered[T any](ctx context.Context, list []T) <-chan T {
+	outStream := make(chan T, len(list))
+	go generate(ctx, list, outStream)
+	return outStream
+}
+
+func generate[T any](ctx context.Context, list []T, outStream chan<- T) {
+	defer close(outStream)
+	for _, item := range list {
+		select {
+		case <-ctx.Done():
+			return
+		case outStream <- item:
+		}
+	}
 }
