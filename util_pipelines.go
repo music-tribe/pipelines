d682eed5d7b13e0440c5866dbd001effa24ea736
John Hughes <john.hughes@musictribe.com>
2023-04-17 12:02:42 +0100

    test: :white_check_mark: Update tests for `OrDone` pipeline
    Add context cancel test to `TeeSplitter`

--- util_pipelines.go
+++ util_pipelines.go
@@ -2,11 +2,14 @@ package pipelines
 
 import (
 	"context"
-	"fmt"
 )
 
 func OrDone[R any](ctx context.Context, inStream <-chan R) <-chan R {
 	outStream := make(chan R)
+	if inStream == nil {
+		close(outStream)
+		panic("OrDone: the provided inStream argument has nil value")
+	}
 
 	go func() {
 		defer close(outStream)
@@ -32,19 +35,20 @@ func OrDone[R any](ctx context.Context, inStream <-chan R) <-chan R {
 
 // TeeSplitter should take a single stream and return 2 identical copies of it
 func TeeSplitter[R any](ctx context.Context, inStream <-chan R) (_, _ <-chan R) {
-	if inStream == nil {
-		panic("TeeSplitter: the provided inStream argument has nil value")
-	}
-
 	outStream1 := make(chan R)
 	outStream2 := make(chan R)
+
+	if inStream == nil {
+		close(outStream1)
+		close(outStream2)
+		panic("TeeSplitter: the provided inStream argument has nil value")
+	}
 
 	go func() {
 		defer close(outStream1)
 		defer close(outStream2)
 
 		for item := range OrDone(ctx, inStream) {
-			fmt.Println(item)
 			var outStream1, outStream2 = outStream1, outStream2
 
 			for i := 0; i < 2; i++ {
--- util_pipelines_test.go
+++ util_pipelines_test.go
@@ -6,6 +6,43 @@ import (
 	"testing"
 )
 
+func TestOrDone(t *testing.T) {
+	t.Run("when the inStream has a nil value, then the method should panic", func(t *testing.T) {
+		defer func() {
+			if perr := recover(); perr == nil {
+				t.Error("expected the method to panic")
+			}
+		}()
+		_ = OrDone[string](context.Background(), nil)
+	})
+
+	t.Run("when we cancel the context before the OrDone pipeline, we should receive a closed channel with less than one item in there", func(t *testing.T) {
+		list := []string{"something", "nother", "another"}
+		ctx, cancel := context.WithCancel(context.Background())
+		inStream := GenerateFromSlice(ctx, list)
+		cancel()
+		outStream := OrDone(ctx, inStream)
+		expectStreamLengthToBeLessThan(2, outStream, t)
+		expectClosedChannel(true, outStream, t)
+	})
+
+	t.Run("when we pass a single item into the stream, we should receive a single item back", func(t *testing.T) {
+		list := []string{"something"}
+		ctx := context.Background()
+		outStream := OrDone(ctx, GenerateFromSlice(ctx, list))
+		expectStreamLengthToBe(len(list), outStream, t)
+		expectClosedChannel(true, outStream, t)
+	})
+
+	t.Run("when we pass multiple values into the stream, we should receive those same values back", func(t *testing.T) {
+		list := []string{"something", "nother", "another"}
+		ctx := context.Background()
+		outStream := OrDone(ctx, GenerateFromSlice(ctx, list))
+		expectStreamLengthToBe(len(list), outStream, t)
+		expectClosedChannel(true, outStream, t)
+	})
+}
+
 func TestTeeSplitter(t *testing.T) {
 	t.Run("when the inStream has a nil value, then the method should panic", func(t *testing.T) {
 		defer func() {
@@ -14,6 +51,20 @@ func TestTeeSplitter(t *testing.T) {
 			}
 		}()
 		_, _ = TeeSplitter[string](context.Background(), nil)
+	})
+
+	t.Run("when we cancel the context before the TeeSplitter pipeline, we should receive 2 closed channel with containing less than one item", func(t *testing.T) {
+		list := []string{"something", "nother", "another"}
+		ctx, cancel := context.WithCancel(context.Background())
+		inStream := GenerateFromSlice(ctx, list)
+		cancel()
+		o1, o2 := TeeSplitter(ctx, inStream)
+		cnts := countAllStreamLengths(o1, o2)
+		for _, cnt := range cnts {
+			if cnt > 2 {
+				t.Errorf("expected less than one item but got %d", cnt)
+			}
+		}
 	})
 
 	t.Run("when we provide an empty stream, we should recieve 2 empty closed streams", func(t *testing.T) {
@@ -30,28 +81,10 @@ func TestTeeSplitter(t *testing.T) {
 		list := []string{"hello"}
 		ctx := context.Background()
 		o1, o2 := TeeSplitter(ctx, GenerateFromSlice(ctx, list))
-		var cnt1, cnt2 int
-		wg := sync.WaitGroup{}
+		cnts := countAllStreamLengths(o1, o2)
 
-		wg.Add(2)
-		go func() {
-			defer wg.Done()
-			for range o1 {
-				cnt1++
-			}
-		}()
-
-		go func() {
-			defer wg.Done()
-			for range o2 {
-				cnt2++
-			}
-		}()
-
-		wg.Wait()
-
-		if cnt1 != len(list) || cnt1 != cnt2 {
-			t.Errorf("expected both stream length to be %d but stream 1 length was %d and stream 2 length was %d", len(list), cnt1, cnt2)
+		if cnts[0] != len(list) || cnts[0] != cnts[1] {
+			t.Errorf("expected both stream length to be %d but stream 1 length was %d and stream 2 length was %d", len(list), cnts[0], cnts[1])
 		}
 		expectClosedChannel(true, o1, t)
 		expectClosedChannel(true, o2, t)
@@ -90,4 +123,22 @@ func TestTeeSplitter(t *testing.T) {
 		expectClosedChannel(true, o1, t)
 		expectClosedChannel(true, o2, t)
 	})
+}
+
+func countAllStreamLengths[T any](streams ...(<-chan T)) []int {
+	wg := sync.WaitGroup{}
+	wg.Add(len(streams))
+	counters := make([]int, len(streams))
+
+	for idx, stream := range streams {
+		go func(idx int, stream <-chan T) {
+			defer wg.Done()
+			for range stream {
+				counters[idx]++
+			}
+		}(idx, stream)
+	}
+
+	wg.Wait()
+	return counters
 }
